77ac0688415ac816b7cfb934d4f3da9d162ff698
AnjaTegel <112825545+AnjaTegel@users.noreply.github.com>
2022-10-01 23:19:55 +0200

    Upload

---
+++ main.py
@@ -0,0 +1,15 @@
+mood = input ("How do you feel today?: ")
+
+
+if mood == "happy":
+    print ("It is great to see you happy!")
+elif mood == "nervous":
+    print ("Take a deep breath 3 times")
+elif mood == "sad":
+    print ("Cheer up!")
+elif mood == "excited":
+    print ("It is great to see you like that")
+elif mood == "relaxed":
+    print ("Would you like to have a drink?")
+else:
+    print ("I don't recognize this mood")
